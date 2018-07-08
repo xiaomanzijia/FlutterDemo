f4d78d2c5134fc3b52b04b0ac70bd600479555e9
changyuan <823354103@qq.com>
2018-07-08 17:56:10 +0800

    layout

---
+++ lib/layout.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new LayoutApp());
+
+class LayoutApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(child:
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text(
+                    "Oeschinen Lake Campground",
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                new Text(
+                  "Kandersteg, Switzerlang",
+                  style: new TextStyle(
+                    color: Colors.grey[500]
+                  ),
+                )
+              ],
+            )
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text("41"),
+        ],
+      ),
+    );
+
+
+    Column buildButtonColum(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Icon(icon, color: color),
+          new Container(
+            margin: const EdgeInsets.only(
+              top: 8.0
+            ),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColum(Icons.call, "CALL"),
+          buildButtonColum(Icons.near_me, "ROUTE"),
+          buildButtonColum(Icons.share, "SHARE")
+        ],
+      ),
+    );
+
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+        Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+
+
+
+    return new MaterialApp(
+      title: "Flutter Demo",
+      theme: new ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Top Lakes"),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Image.asset(
+              "images/lake.jpg",
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        ),
+      ),
+    );
+  }
+
+}
